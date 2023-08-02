79885fb876c971d7e448079f4bb524ee25c59de7
anvh-dev-iotlink <andeptrai89@gmail.com>
2023-08-03 06:18:00 +0700

    add mui icon

--- components/theme/themeSwitcher.tsx
+++ components/theme/themeSwitcher.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-
+import ModeNightIcon from '@mui/icons-material/ModeNight';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 export const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
@@ -23,7 +24,7 @@ export const ThemeSwitcher = () => {
       className={`w-fit right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "light" ? "Dark" : "Light"}
+      {theme === "light" ? <ModeNightIcon/> : <WbSunnyIcon/>}
     </button>
   );
 };

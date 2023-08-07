e13763a42424bac86a301bfae8f32213a5f2b5bc
anvh-dev-iotlink <andeptrai89@gmail.com>
2023-08-07 09:51:24 +0700

    fix home page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,7 +2,7 @@ import HotBlog from "@/components/home/hotBlog";
 import Topics from "@/components/home/topic";
 import { Locale } from "@/i18next.config";
 
-export default function Home({lang}: {lang: Locale}) {
+export default function Home() {
   
   return (
     <div>
